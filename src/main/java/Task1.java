f74afea071b15f207b9ac1b5e1d35ee20a4e0478
lubov <loveavduhova@gmail.com>
2022-11-06 12:13:12 +0700

    homework done

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,6 +1,3 @@
-import java.util.LinkedList;
-import java.util.List;
-
 public class Task1 {
     public static void main(String[] args) {
         Tower(3, "1", "3", "2");
@@ -11,6 +8,7 @@ public class Task1 {
             System.out.printf("%d from %s to %s", n, source, target);
             System.out.println();
             return;
+
         }
         Tower(n - 1, source, buffer, target);
         System.out.printf("%d from %s to %s", n, source, target);
