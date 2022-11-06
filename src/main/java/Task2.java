09aaadc8127ac8c8c47214e6c240e304c25c835c
lubov <loveavduhova@gmail.com>
2022-11-06 12:12:07 +0700

    homework done

---
+++ src/main/java/Task2.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 7, 1, 9, 3, 8};
+        System.out.println(Arrays.toString(insertionSort(arr)));
+
+    }
+
+    public static int[] insertionSort(int[] sortArr) {
+        int j;
+        for (int i = 1; i < sortArr.length; i++) {
+            int swap = sortArr[i];
+            for (j = i; j > 0 && swap < sortArr[j - 1]; j--) {
+                sortArr[j] = sortArr[j - 1];
+            }
+            sortArr[j] = swap;
+        }
+        return sortArr;
+    }
+}
